4cb24637744b67bce521773f732c19ed41d7e2b8
Till Kottmann <deletescape@gmail.com>
2018-03-27 10:46:36 +0200

    maybe this will work

---
+++ TextGit/Document.cs
@@ -0,0 +1,21 @@
+namespace TextGit
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Document
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "text")]
+        public string DocumentContent { get; set; }
+    }
+}
